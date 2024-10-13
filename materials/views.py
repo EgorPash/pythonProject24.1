c6771f718ce12caa64e67c2aeb2e44f17fb01d75
egorpash <2021104302@pnu.edu.ru>
2024-10-13 16:20:30 +0200

    Ошибки исправлены

--- materials/views.py
+++ materials/views.py
@@ -1,11 +1,11 @@
-from rest_framework import viewsets
-from materials.models import Course
-from materials.serializers import CourseSerializer
 from rest_framework import generics
-from materials.models import Lesson
-from .serializers import LessonSerializer
-from users.permissions import IsModerator, IsOwner
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from materials.models import Course
+from materials.models import Lesson
+from materials.serializers import CourseSerializer
+from users.permissions import IsOwner, IsModerator
+from .serializers import LessonSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -14,28 +14,21 @@ class CourseViewSet(viewsets.ModelViewSet):
 
     def get_permissions(self):
         if self.action in ['create', 'update', 'destroy']:
-            self.permission_classes = [IsAuthenticated, IsOwner]
+            self.permission_classes = [IsAuthenticated, IsOwner | IsModerator]  # Владельцы или менеджеры
         else:
-            self.permission_classes = [IsAuthenticated | IsOwner]  # Для просмотра списка
+            self.permission_classes = [IsAuthenticated]  # Для просмотра списка
         return super().get_permissions()
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+        serializer.save(owner=self.request.user)  # Устанавливаем владельца
 
 
 class LessonListCreateAPIView(generics.ListCreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
-    def __init__(self, **kwargs):
-        super().__init__(kwargs)
-        self.action = None
-
     def get_permissions(self):
-        if self.action in ['create', 'update', 'destroy']:
-            self.permission_classes = [IsAuthenticated, IsOwner]
-        else:
-            self.permission_classes = [IsAuthenticated | IsOwner]  # Для просмотра списка
+        self.permission_classes = [IsAuthenticated]  # Только авторизованные пользователи
         return super().get_permissions()
 
     def perform_create(self, serializer):
@@ -44,3 +37,7 @@ class LessonListCreateAPIView(generics.ListCreateAPIView):
 class LessonRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, IsOwner]  # Только владельцы могут обновлять и удалять
+
+    def perform_update(self, serializer):
+        serializer.save(owner=self.request.user)  # Убедитесь, что владелец установлен при обновлении
